3e2a9a877b819146a44f2e1d0ad91a55c607452c
Prodeon <prodeon21@gmail.com>
2025-12-11 14:23:50 +0300

    В валидации исправлен 0 на "Не указано"

--- src/Vacancy.py
+++ src/Vacancy.py
@@ -7,13 +7,13 @@ class Vacancy:
     Так же проверяем указано ли название. Если нет, то прописываем Без названия.
     """
 
-    __slots__ = ("title", "link", "salary", "description")
+    __slots__ = ("title", "link", "salary_from","salary_to", "description")
 
-    def __init__(self, title: str, link: str, salary: int | float, description: str):
+    def __init__(self, title: str, link: str, salary_from: int | float, salary_to: int | float):
         self.title = self.__validate_title(title)
         self.link = link
-        self.salary = self.__validate_salary(salary)
-        self.description = description
+        self.salary_from = self.__validate_salary(salary_from)
+        self.salary_to = self.__validate_salary(salary_to)
 
     def __gt__(self, other):
         if isinstance(other, Vacancy):
@@ -53,8 +53,8 @@ class Vacancy:
 
     @staticmethod
     def __validate_salary(salary: int | float):
-        if salary is None or isinstance(salary, str):
-            salary = 0
+        if salary is None:
+            salary = "Не указано"
         else:
             salary = salary
         return salary
