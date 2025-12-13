5a04e5ff584741f9014e1684a42ba45386d70fe3
Prodeon <prodeon21@gmail.com>
2025-12-13 09:50:04 +0300

    Доработка основной функции, рефакторинг имеющегося кода

--- main.py
+++ main.py
@@ -1,18 +1,24 @@
 from src.connector import Connector, HeadHunterApi
-from utils.functions import ranged_vacancies, top_n
+from utils.functions import ranged_vacancies, top_n, print_vacancies
 from src.Vacancy import Vacancy
 
 hh_api = HeadHunterApi("https://api.hh.ru/vacancies", {"User-Agent" : "test for skypro"})
 
 
 def main():
-    target = sorted(list(map(int,input("""По каким критериям отфильтровать вакансии?
+    while True:
+        try:
+            target = sorted(list(map(int, input("""По каким критериям отфильтровать вакансии?
 1 - ключевые слова
 2 - по диапазону зарплат
 3 - по самым высоким зарплатам
 4 - удалёнка или офис
 (Напишите в одну строку через пробел)
-""").split())))
+""").strip().split())))
+        except ValueError:
+            continue
+        if all(number in (1, 2, 3, 4) for number in target):
+            break
 
     if 1 in target:
         name = input("Введите ключевые слова: ")
@@ -20,8 +26,22 @@ def main():
     else:
         vacancies = hh_api.get_vacancies()
 
-    if 2 in target or 3 in target:
-        vacancies = Vacancy.cast_to_object_list(vacancies)
+    if 4 in target:
+        work_f = input("1 - для поиска удалёнки, 2 - для поиска офиса, 3 - гибрид")
+        while work_f not in ("1", "2", "3"):
+            work_f = input("1 - для поиска удалёнки, 2 - для поиска офиса, 3 - гибрид")
+        if work_f == "1":
+            vacancies = [vacancy for vacancy in vacancies if
+                         any(work_format["id"] == "REMOTE" for work_format in vacancy["work_format"])]
+        if work_f == "2":
+            vacancies = [vacancy for vacancy in vacancies if
+                         any(work_format["id"] == "ON_SITE" for work_format in vacancy["work_format"])]
+        if work_f == "3":
+            vacancies = [vacancy for vacancy in vacancies if
+                         any(work_format["id"] == "HYBRID" for work_format in vacancy["work_format"])]
+
+
+    vacancies = Vacancy.cast_to_object_list(vacancies)
 
     if 2 in target:
         while True:
@@ -53,6 +73,8 @@ def main():
 
 
 
+# Сделать перелистывание страницы
+
 
 
 
@@ -60,4 +82,4 @@ def main():
 
 
 if __name__ == "__main__":
-    print(main())
+    print_vacancies(main())
