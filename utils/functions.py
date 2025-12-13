435f858396d29398005e24560aeb1f8ffa08967c
Prodeon <prodeon21@gmail.com>
2025-12-13 09:49:06 +0300

    Исправлена функция для отображения топа вакансий, а так же диапазон зарплат. Добавлена функция для принта вакансий

--- utils/functions.py
+++ utils/functions.py
@@ -5,20 +5,35 @@ def top_n(vacancies : list[Vacancy], n : int):
     """Функция для отображения топовых вакансий по зарплате.
     Количество отображения регулируется параметром n"""
 
-    top_vacancies = sorted(vacancies, key=lambda x: x.salary_from)
+    # top_vacancies = sorted(vacancies, key=lambda x: x.salary_from)
+    #
+    # top_n_vacancies = [top_vacancies[i] for i in range(n)]
 
-    top_n_vacancies = [top_vacancies[i] for i in range(n)]
+    length = len(vacancies)
+    for i in range(length):
+        for j in range(0, length - i - 1):
+            if vacancies[j] < vacancies[j + 1]:
+                vacancies[j], vacancies[j + 1] = vacancies[j + 1], vacancies[j]
 
 
-    return top_n_vacancies
+    return vacancies[:n]
 
 def ranged_vacancies(vacancies : list[Vacancy], range_from : int | float, range_to : int | float = None):
     """Функция для отображения вакансий в выбранном диапазоне зарплат."""
 
-    if not range_to:
-        ranged_vacancies = [vacancy for vacancy in vacancies if vacancy.salary_from >= range_from]
+    if range_to is None:
+        ranged_vacancies = [vacancy for vacancy in vacancies if vacancy >= range_from]
     else:
-        ranged_vacancies = [vacancy for vacancy in vacancies if vacancy.salary_from >= range_from
-                            and vacancy.salary_to <= range_to]
+        ranged_vacancies = [vacancy for vacancy in vacancies if range_from <= vacancy <= range_to]
 
     return ranged_vacancies
+
+def print_vacancies(vacancies : list[Vacancy]) -> str:
+
+    resulted_string = ""
+
+    for vacancy in vacancies:
+        string = f"{vacancy.title} От: {vacancy.salary_from}, до : {vacancy.salary_to}, {vacancy.link}\n"
+        resulted_string += string
+
+    print(resulted_string)
